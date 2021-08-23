02a78922183756feac23b272b0935d46bb318df9
ThaSMorato <tha.s.morato@gmail.com>
2021-08-23 19:35:18 -0300

    feat: ensure remoteAuthentication returns an account on success

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,17 +1,17 @@
-import { AuthenticationParams } from "@/domain/usecases/authentication";
+import { Authentication, AuthenticationParams } from "@/domain/usecases/authentication";
 import { HttpPostClient } from "@/data/protocols/http/http-post-client";
 import { HttpStatusCode } from "@/data/protocols/http/http-response";
 import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials-error";
 import { UnexpectedError } from "@/domain/errors/unexpected-credentials-error";
 import { AccountModel } from "@/domain/models/account-model";
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor(
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>
   ) {}
 
-  async auth(params: AuthenticationParams): Promise<void> {
+  async auth(params: AuthenticationParams): Promise<AccountModel> {
     const { url } = this;
     const httpResponse = await this.httpPostClient.post({ url, body: params });
 
@@ -19,7 +19,7 @@ export class RemoteAuthentication {
       case HttpStatusCode.unathorized:
         throw new InvalidCredentialsError();
       case HttpStatusCode.ok:
-        return Promise.resolve();
+        return httpResponse.body;
       default:
         throw new UnexpectedError();
     }
